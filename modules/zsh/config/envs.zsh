6d2d1e027d548f2137b34d07256e344db29118ac
Alejandro Banderas <alejandro.banderas@me.com>
2025-10-03 21:01:49 -0500

    ssh-agent

--- modules/zsh/config/envs.zsh
+++ modules/zsh/config/envs.zsh
@@ -1,16 +1,16 @@
-# Add to PATH:
+# -- Add to path
 export PATH="$HOME/.local/bin:$PATH" # TODO: Omarchy sets this in uwsm
 
-# History options:
+# -- zsh istory options
 mkdir -p ~/.cache/zsh
 export HISTSIZE=10000
 export SAVEHIST="${HISTSIZE}"
 export HISTFILE=~/.cache/zsh/history  # be careful not to include this file on any git repo
 
 
-# XDG Base Directory:
-#   Only XDG_RUNTIME_DIR is set by default through pam_systemd(8). It is up to the user to explicitly define the other variables according to the specification.
-#   https://wiki.archlinux.org/title/XDG_Base_Directory#User_directories
+# -- TODO: XDG Base Directory
+#    Only XDG_RUNTIME_DIR is set by default through pam_systemd(8). It is up to the user to explicitly define the other variables according to the specification.
+#    https://wiki.archlinux.org/title/XDG_Base_Directory#User_directories
 # export XDG_CONFIG_HOME="$HOME/.config"  # $HOME value is taken from `/etc/passwd`
 # export XDG_CACHE_HOME="$HOME/.cache"
 # export XDG_DATA_HOME="$HOME/.local/share"
@@ -19,15 +19,20 @@ export HISTFILE=~/.cache/zsh/history  # be careful not to include this file on a
 # export XDG_CONFIG_DIRS="/etc/xdg"
 
 
-# SSH-agent:
-#   Required for the ssh-agent to work across terminals
-# export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"  # https://wiki.archlinux.org/title/SSH_keys#Start_ssh-agent_with_systemd_user
+# -- ssh-agent
+#    Environment variable that points to a Unix socket file created by the
+#    ssh-agent. That socket is the communication channel between your
+#    applications and the running ssh-agent. When you run ssh, it checks this
+#    variable to know where to talk to the agent.
+# export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"  # -- In Archlinux
+export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent"  # -- In NixOS
 
 
-# Defaults:
+# -- TODO: Env variables for default programs:
+#   Omarchy set these with uwsm.
 #   These variables define default applications when executing commands from the terminal.
 #   Do not set the `TERM` variable!, it is set by each terminal emulator.
-export EDITOR="nvim"  # TODO: Omarchy sets this in uwsm
+export EDITOR="nvim"
 export SUDO_EDITOR="$EDITOR"
 export VISUAL="nvim"  # when opening a GUI editor (nnn `-e` option will respect this variable)
 export MANPAGER="less -R --use-color -Dd+r -Du+b"
@@ -39,9 +44,11 @@ export LC_ALL="en_US.UTF-8";
 # export BROWSER="brave"  # when opening links
 # export GPG_TTY="${TTY:-$(tty)}"
 
-# Colors:
+
+# -- Themes
 export BAT_THEME="ansi"
 export GTK_THEME="Adwaita:dark"
+
 
 # fzf tokyonight: https://github.com/folke/tokyonight.nvim/tree/main/extras/fzf
 # export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
