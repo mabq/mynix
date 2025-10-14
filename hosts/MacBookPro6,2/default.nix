f3c53978e6a5523736a29d97ac9596fc36364e35
mabq <alejandro.banderas@me.com>
2025-10-14 18:54:55 -0500

    Move bluetooth to shared host settings

--- hosts/MacBookPro6,2/default.nix
+++ hosts/MacBookPro6,2/default.nix
@@ -11,27 +11,4 @@
   # - Use `ip a` to check network interfaces on each host.
   networking.interfaces.enp2s0.useDHCP = true;
   networking.interfaces.wls1b1.useDHCP = true;
-
-  # --- bluetooth ---
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        # - Shows battery charge of connected devices on supported Bluetooth
-        #   adapters. Defaults to 'false'.
-        Experimental = true;
-        # - When enabled other devices can connect faster to us, however
-        #   the tradeoff is increased power consumption. Defaults to 'false'.
-        FastConnectable = true;
-      };
-      Policy = {
-        # - Enable all controllers when they are found. This includes adapters
-        #   present on start as well as adapters that are plugged in later on.
-        #   Defaults to 'true'.
-        AutoEnable = true;
-      };
-    };
-  };
 }
