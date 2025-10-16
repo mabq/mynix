d2a8f72c32b9881fd5024c3ce0150bc08d7bfe67
mabq <alejandro.banderas@me.com>
2025-10-15 20:51:13 -0500

    removed fixed network interfaces

--- hosts/GB-BXi3-5010/default.nix
+++ hosts/GB-BXi3-5010/default.nix
@@ -7,7 +7,4 @@
   # --- networking ---
 
   networking.hostName = "nuc";
-
-  # - Use `ip a` to check network interfaces on each host.
-  networking.interfaces.enp1s0.useDHCP = true;
 }
--- hosts/MacBookPro6,2/default.nix
+++ hosts/MacBookPro6,2/default.nix
@@ -7,8 +7,4 @@
   # --- networking ---
 
   networking.hostName = "macbook";
-
-  # - Use `ip a` to check network interfaces on each host.
-  networking.interfaces.enp2s0.useDHCP = true;
-  networking.interfaces.wls1b1.useDHCP = true;
 }
