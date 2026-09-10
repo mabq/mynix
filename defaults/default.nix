a8a7c5a5dcde9fcf1c81f6d85e13cadc54f2586c
mabq <alejandro.banderas@me.com>
2026-09-09 21:16:33 -0500

    all

--- defaults/default.nix
+++ defaults/default.nix
@@ -5,7 +5,6 @@
   pkgs,
   host,
   user,
-  profile,
   repoBranch,
   repoName,
   repoUrl,
@@ -34,7 +33,7 @@ with lib;
       User = "${user}";
       ExecStart = [
         # systemd requires absolute paths to executables
-        "${pkgs.git}/bin/git clone ${repoUrl}.git ${repoDir}"
+        "${pkgs.git}/bin/git clone ${repoUrl} ${repoDir}"
         "${pkgs.git}/bin/git -C ${repoDir} checkout ${repoBranch}"
       ];
     };
