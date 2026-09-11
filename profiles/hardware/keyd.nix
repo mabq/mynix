d043def3e8fb810542eb75c9676ede7e6497d16d
mabq <alejandro.banderas@me.com>
2026-09-10 22:01:29 -0500

    all

--- profiles/hardware/keyd.nix
+++ profiles/hardware/keyd.nix
@@ -4,26 +4,17 @@
 {
   self,
   pkgs,
-  user,
   repoConfigDir,
   ...
 }:
 {
   # The keyd systemd service runs as root. It captures your input events,
-  # remaps them, and emits them to the system at a low level. It does not
-  # require your user account to be in any group for the remaps themselves to
-  # function.
+  # remaps them, and emits them to the system at a low level.
   services.keyd.enable = true;
-
-  # If you want to use non-root CLI tools (like `keyd monitor` to debug
-  # keycodes), you need to explicitly declare the group in your NixOS config
-  # and add your user to it.
-  # users.groups."keyd" = { };
-  # users.users.${user}.extraGroups = [ "keyd" ];
 
   environment = {
     systemPackages = [
-      # This package is required to debug keycodes with `keyd monitor`
+      # Required to debug keycodes with `sudo keyd monitor`
       pkgs.keyd # Key remapping daemon for Linux
     ];
 
