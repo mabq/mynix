1e01930142bf8c19ea71fbbd2b38fe2afe3d2825
mabq <alejandro.banderas@me.com>
2026-09-11 18:36:50 -0500

    all

--- lib/mkSystem.nix
+++ lib/mkSystem.nix
@@ -58,7 +58,7 @@ inputs.nixpkgs.lib.nixosSystem {
     inputs.sops-nix.nixosModules.sops
 
     # -- Configs --
-    ../secrets/sops.nix
+    # ../secrets/sops.nix
     ../defaults
     ../hosts/${host}.nix
     ../users/${user}.nix
