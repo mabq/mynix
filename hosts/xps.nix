32282cb3b4be320be84038a83930224acec361c8
mabq <alejandro.banderas@me.com>
2026-09-15 00:54:00 +0000

    facter for xps

--- hosts/xps.nix
+++ hosts/xps.nix
@@ -18,6 +18,11 @@
   #  Newer versions of NixOS could take better decisions with the same report.
   hardware.facter.reportPath = ./facter/${host}.json; # [1]
 
+  # Installer NixOS version
+  #  Set it once at installation and never change it again.
+  #  Use `nixos-version` on the ISO to check its version.
+  system.stateVersion = "26.05";
+
   # Grub
   #  Works for both EFI and BIOS systems. It's not necessary to set
   #  `boot.loader.grub.device` here, Disko will take care of that.
@@ -34,9 +39,4 @@
   #   systemd-boot.enable = true;
   #   efi.canTouchEfiVariables = true;
   # };
-
-  # Installer NixOS version
-  #  Set it once at installation and never change it again.
-  #  Use `nixos-version` on the ISO to check its version.
-  system.stateVersion = "26.05";
 }
