f14837b46940958bf36f3fd4af94c818621cfea1
mabq <alejandro.banderas@me.com>
2026-09-17 09:58:11 -0500

    all

--- secrets/default.nix
+++ secrets/default.nix
@@ -84,7 +84,7 @@ in
   # Don't use the home-manager module, you would not be able to access secrets
   # from NixOS options (like tailscale).
   sops = lib.mkIf hasSecrets {
-    package = pkgs.sops;
+    # package = pkgs.sops;
     age.keyFile = ageKeyFile;
     defaultSopsFile = secretsFile;
     # This creates an attribute set where the keys are the secret's names and
