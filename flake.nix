177df82a049c2e05d8b4110107c04188bd026a20
mabq <alejandro.banderas@me.com>
2026-09-17 12:46:44 -0500

    all

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable"; # [1]
-      # url = "github:NixOS/nixpkgs/nixos-26.05"; # [1]
+      # url = "github:NixOS/nixpkgs/nixos-unstable"; # [1]
+      url = "github:NixOS/nixpkgs/nixos-26.05"; # [1]
     };
 
     home-manager = {
